f1beb1217f9f2e3ae801a20dfd87fd4a147cb78d
Raul <raul@liftel.es>
2023-03-28 10:44:03 +0200

    Create hero repository and her service

---
+++ src/main/java/es/sarman/pruebatecnica/Heroes/HeroRepository.java
@@ -0,0 +1,6 @@
+package es.sarman.pruebatecnica.Heroes;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HeroRepository extends CrudRepository<Hero, Integer> {
+}
---
+++ src/main/java/es/sarman/pruebatecnica/Heroes/IHeroService.java
@@ -0,0 +1,20 @@
+package es.sarman.pruebatecnica.Heroes;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IHeroService {
+
+    List<Hero> getHeroes();
+
+    Hero getHero(int id);
+
+    List<Hero> searchHeroes(String query);
+
+    Hero updateHero(int id, HeroDTO update);
+
+    boolean removeHero(int id);
+
+}
