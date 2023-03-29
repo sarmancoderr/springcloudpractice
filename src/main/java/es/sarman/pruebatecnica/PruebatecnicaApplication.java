956c3c0d04aba6413131cc95085999c8bee8b9e9
Raul <raul@liftel.es>
2023-03-29 12:11:01 +0200

    Añadir autenticación básica

--- src/main/java/es/sarman/pruebatecnica/PruebatecnicaApplication.java
+++ src/main/java/es/sarman/pruebatecnica/PruebatecnicaApplication.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableWebSecurity
 public class PruebatecnicaApplication {
 
 	Logger logger = LoggerFactory.getLogger(PruebatecnicaApplication.class);
---
+++ src/main/java/es/sarman/pruebatecnica/SecurityConfig.java
@@ -0,0 +1,27 @@
+package es.sarman.pruebatecnica;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/heroes/*").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
